5250584ec5ef4ebed8c0f7df0a147be7626074df
Arthur Santos Madureira <127901436+arthurTutz@users.noreply.github.com>
2023-06-09 11:19:13 -0300

    Update jogadores.js

--- codigo/js/jogadores.js
+++ codigo/js/jogadores.js
@@ -1,59 +1,11 @@
-function leDados () {
-    let strDados = localStorage.getItem('db');
-    letobjDados = {};
+let bancoUsuarios = JSON.parse(localStorage.getItem("dadosUsuarios"));
+function exibirUsuario() {
+    var card = "";
+    for (let i = 0; i < bancoUsuarios.length; i++) {
+        const usuario = bancoUsuarios[i];
 
-    if(strDados) {
-        objDados = JSON.parse (strDados);
+        card += `<p>${usuario.userName - usuario.bio}}</p>
+
     }
-    else{
-        objDados = { login: [ 
-         {email: "joãogamertopeira@gmail.com" , senha: "joaolutador123"},
-         {email: "arthurzinmafioso@gmail.com" , senha: "Coxinha123"},
-         {email: "mateuslalau007@gmail.com" , senha: "cruzeiro123"} ]
-        }
-    }
-    return objDados;
-
+    docu
 }
-
-function salvaDados (dados){
-    localStorage.setItem ('db' ,JSON.stringify (dados));
-
-}
-
-function incluirContato () {
-    let objDados = leDados();
-
-    let email = document.getElementById('emailLogin').value;
-    let senha = document.getElementById('senhaLogin').value;
-    let novoContato = {
-        xemail: email,
-        xsenha: senha
-
-    };
-    objDados.login.push (novoContato);
-
-    salvaDados (objDados);
-
-    imprimeDados();
-   
-   
-
-}
-
-function imprimeDados () {
-    let tela = document.getElementById('tela');
-    let strHtml = '';
-    let objDados = leDados ();
-
-    for(i = 0; i< objDados.login.length; i++){
-        strHtml += '<p>${objDados.login[i].email - ${objDados.login[i].senha}</p>'
-    }
-    tela.innerHTML = strHtml;
-
-
-}
-
-//configura os botões
-document.getElementById ('emailLogin').addEventListener('click' , imprimeDados)
-document.getElementById ('senhaLogin').addEventListener('click' , imprimeDados)
