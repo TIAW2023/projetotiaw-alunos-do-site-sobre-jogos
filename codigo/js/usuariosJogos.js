63e59b221b42888409d6f9fec74f7f66e2b6f212
Matheus Felipe da Silva <matheusfelipe.silva160205@gmail.com>
2023-06-14 22:58:21 -0300

    update

--- codigo/js/usuariosJogos.js
+++ codigo/js/usuariosJogos.js
@@ -20,19 +20,21 @@ function renderizarJogos(jogos) {
       const jogoAtual = jogos[i];
   
       divJogos.innerHTML += `
-      <div class="jogo">
-        <div>
-        <img src="${jogoAtual.thumbnail}"/>
+      <a class="jogoContainer" href="infoJogos.html?id=${jogoAtual.id}">
+        <div class="jogo">
+          <div>
+          <img src="${jogoAtual.thumbnail}"/>
+          </div>
+          <div class="texto-jogo">
+          <h1>${jogoAtual.title}</h1>
+          <p>Gênero: ${jogoAtual.genre}</p>
+          <p>Descrição: ${jogoAtual.short_description}</p>
+          <p>Data de Lançamento: ${new Date(
+            jogoAtual.release_date.replace("-", "/")
+          ).toLocaleDateString("pt-BR")}</p>
+          </div>
         </div>
-        <div class="texto-jogo">
-        <h1>${jogoAtual.title}</h1>
-        <p>Gênero: ${jogoAtual.genre}</p>
-        <p>Descrição: ${jogoAtual.short_description}</p>
-        <p>Data de Lançamento: ${new Date(
-          jogoAtual.release_date.replace("-", "/")
-        ).toLocaleDateString("pt-BR")}</p>
-        </div>
-      </div>  
+      </a>  
       `;
     }
 }
