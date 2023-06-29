ce83d4b8ce0a88893569d0b6846aa30e827dc71f
Arthur Santos Madureira <127901436+arthurTutz@users.noreply.github.com>
2023-06-29 18:53:43 -0300

    up

--- codigo/js/script.js
+++ codigo/js/script.js
@@ -41,6 +41,45 @@ function clicado(){
     }
 
 }
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "/img/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "/img/close_white_36dp.svg";
+    }
+}
+
+const carousel = document.querySelector('.carousel');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+let slidePosition = 0;
+
+prevButton.addEventListener('click', () => {
+  moveToPrevSlide();
+});
+
+nextButton.addEventListener('click', () => {
+  moveToNextSlide();
+});
+
+function updateSlidePosition() {
+  carousel.style.transform = `translateX(${slidePosition}px)`;
+}
+
+function moveToPrevSlide() {
+  if (slidePosition === 0) return;
+  slidePosition += carousel.offsetWidth;
+  updateSlidePosition();
+}
+
+function moveToNextSlide() {
+  if (slidePosition === -(carousel.offsetWidth * (carousel.childElementCount - 1))) return;
+  slidePosition -= carousel.offsetWidth;
+  updateSlidePosition();
+}
 
 btnNext.addEventListener('click', nextSlider)
 btnPrev.addEventListener('click', prevSlider)
